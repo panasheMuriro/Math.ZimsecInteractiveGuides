93af67b46c429818d91dedc2c527bb2c9e9c6be1
murirop <panashemuriro2021@gmail.com>
2025-07-18 12:50:01 +0200

    minor ui

--- src/components/Topic/interactive/Topic2/SetBasics.tsx
+++ src/components/Topic/interactive/Topic2/SetBasics.tsx
@@ -159,7 +159,7 @@ const SetBasics = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-[#EFE4D2] rounded-lg shadow-lg">
+    <div className="max-w-md mx-auto p-4 bg-[#FF9B45] rounded-lg shadow-lg">
       {/* Header */}
       <div className="mb-4">
         <div className="flex justify-between items-center mb-2">
