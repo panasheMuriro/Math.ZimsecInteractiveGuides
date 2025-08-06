c57c9fa9aa116812e5a2462b9b7ac7c879867a53
murirop <panashemuriro2021@gmail.com>
2025-08-06 09:51:48 +0200

    setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 // import checker from 'vite-plugin-checker';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss() ],
+    plugins: [react(), tailwindcss(), VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Math | Zimsec Interactive Guides',
+        short_name: 'O Math Zim',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#e07a5f',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      }})],
 
-  // plugins: [react()],
 })
