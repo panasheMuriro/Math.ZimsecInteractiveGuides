3fea74fa8acacc2cad83f59a2ee67f9669c9c59a
murirop <panashemuriro2021@gmail.com>
2025-08-06 19:23:44 +0200

    scroll to top upon navigating

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import ScrollToTop from './components/Global/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
