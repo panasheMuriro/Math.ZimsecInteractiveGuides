acf3c228d916cbf40e7e3141b8bcebce8906996c
murirop <panashemuriro2021@gmail.com>
2025-08-08 17:30:09 +0200

    add app bar

--- src/components/Topics/TopicLayout.tsx
+++ src/components/Topics/TopicLayout.tsx
@@ -1,13 +1,15 @@
 // components/Topic/TopicLayout.tsx
-import { Outlet, useParams, Navigate } from "react-router-dom";
+import { Outlet, useParams, Navigate, useNavigate } from "react-router-dom";
 import { topics } from "../../data/topics";
 import { useState } from "react";
+import AppBar from "../Global/AppBar";
 
 const TopicLayout = () => {
   const { topicId } = useParams();
   const [completedSections] = useState<Set<number>>(new Set());
   const [quizScore, setQuizScore] = useState(0);
   const topicData = topics.find((t) => t.id === Number(topicId));
+  const navigate = useNavigate();
 
   if (!topicData) {
     return <Navigate to="/" replace />;
@@ -24,6 +26,7 @@ const TopicLayout = () => {
 
   return (
     <div className="font-sans">
+  <AppBar title="" showBackButton onBack={() => navigate(-1)} />
       <Outlet context={contextValue} />
     </div>
   );
